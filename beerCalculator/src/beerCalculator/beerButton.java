06c5ccc7ba0e3429e1ae3ee8b01ed108205f0152
mwg6 <maximgoodman@gmail.com>
2018-03-05 19:14:27 -0500

    Update beerButton.java

--- beerCalculator/src/beerCalculator/beerButton.java
+++ beerCalculator/src/beerCalculator/beerButton.java
@@ -6,14 +6,17 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class beerButton extends JPanel {
+	//create the two buttons needed on the bottom for addition and printing
 	   private JButton addInfoButton = new JButton("Add Ingredient");
 	   private JButton printButton = new JButton("Make Recipe");
-	    
+	   
+	//add these buttons
 	   public beerButton() {
 	      add(addInfoButton);
 	      add(printButton);
 	   }
-	   	    
+	   
+		//enable them to be touched by the world and feel it in their hearts
 	   public void addInfoBtnAddActionListener(ActionListener listener) {
 	      addInfoButton.addActionListener(listener);
 	     
