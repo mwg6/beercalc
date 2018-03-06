6cf5a0c7ede5865700973101eec051ef081d10db
mwg6 <maximgoodman@gmail.com>
2018-03-05 19:30:59 -0500

    Update beerButtonTop.java

--- beerCalculator/src/beerCalculator/beerButtonTop.java
+++ beerCalculator/src/beerCalculator/beerButtonTop.java
@@ -6,13 +6,16 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class beerButtonTop extends JPanel{
+	
+	//top button to banish mistakes
 	private JButton clearButton = new JButton("Clear");
 	  
+	//put it on this model
 	public beerButtonTop() {
 	      add(clearButton);
 	      
 	   }
-	   	    
+	 //let it sing	    
 	   public void addClearBtnAddActionListener(ActionListener listener) {
 	      clearButton.addActionListener(listener);
 	     
