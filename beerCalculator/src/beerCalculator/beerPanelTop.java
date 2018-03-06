2d31edf7652cb06eb58c095659dea0d303318229
mwg6 <maximgoodman@gmail.com>
2018-03-05 19:44:26 -0500

    Update beerPanelTop.java

--- beerCalculator/src/beerCalculator/beerPanelTop.java
+++ beerCalculator/src/beerCalculator/beerPanelTop.java
@@ -7,17 +7,20 @@ import javax.swing.JPanel;
 
 public class beerPanelTop extends JPanel{
 	
+	//Can't have the user doing foolish thingsto the batch size. restricted to 1-10 gallon batches
 	String[] nums = {"1","2","3","4","5","6","7","8","9","10"};
    private JComboBox galOptions = new JComboBox(nums);
 
     
    public beerPanelTop() {
+	   //Make it so 
       add(new JLabel("Gallons:"));
       add(galOptions);
       add(Box.createHorizontalStrut(10));
    }
     
    public int getQuantity() {
+	   //give a method to return batch size
       return Integer.parseInt(galOptions.getSelectedItem().toString());
    }
    
